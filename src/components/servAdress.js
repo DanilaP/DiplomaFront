37d71d1033740876fe15b6482e7ac918a6f1922c
unknown <danilaplatonov209@gmail.com>
2023-07-26 10:00:40 +0300

    bug fixed

--- src/components/servAdress.js
+++ src/components/servAdress.js
@@ -1,4 +1,4 @@
 //http://localhost:5000
 //https://backend-danila123.amvera.io
-let SERVADRESS = "http://localhost:5000";
+let SERVADRESS = "https://backend-danila123.amvera.io";
 export default SERVADRESS;
